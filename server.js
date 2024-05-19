11f34d48092f051f1c88386b60650cb4d842bfc1
max0man0 <0naxman0@gmail.com>
2024-05-19 15:34:14 +0300

    removed cors

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const logResponse = require('./log.js');
-const cors = require('cors');
 
 const app = express();
 const port = 3000;
-
-app.use(cors());
 
 app.get('/:id', (req, res, next) => {
     const id = req.params.id;
